6243deebd7c69227fd3b09ad74eb51cb9d5b1d58
WZNT-KimHyeJin <pcrs5278@naver.com>
2024-12-03 13:32:52 +0900

    init & entity SolvedCode

---
+++ dusty_code/src/test/java/wznt/projects/dusty_code/DustyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package wznt.projects.dusty_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustyCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
